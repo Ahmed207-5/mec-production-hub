16c63ff3c56ce4413173c55bb7a4885b91f64f3a
Ahmed El-Saeed <ahmdmydw981@gmail.com>
2026-08-07 21:14:41 +0300

    Restore production drive links

--- src/data/power.ts
+++ src/data/power.ts
@@ -5,34 +5,10 @@ export const batches: Batch[] = [
     year: 2027,
     label: "دفعة قوى 2027",
     years: [
-      {
-        yearNumber: 1,
-        semesters: {
-          first: "",
-          second: "",
-        },
-      },
-      {
-        yearNumber: 2,
-        semesters: {
-          first: "",
-          second: "",
-        },
-      },
-      {
-        yearNumber: 3,
-        semesters: {
-          first: "",
-          second: "",
-        },
-      },
-      {
-        yearNumber: 4,
-        semesters: {
-          first: "",
-          second: "",
-        },
-      },
+      { yearNumber: 1, semesters: { first: "", second: "" } },
+      { yearNumber: 2, semesters: { first: "", second: "" } },
+      { yearNumber: 3, semesters: { first: "", second: "" } },
+      { yearNumber: 4, semesters: { first: "", second: "" } },
     ],
   },
 
@@ -40,34 +16,43 @@ export const batches: Batch[] = [
     year: 2026,
     label: "دفعة قوى 2026",
     years: [
-      {
-        yearNumber: 1,
-        semesters: {
-          first: "",
-          second: "",
-        },
-      },
-      {
-        yearNumber: 2,
-        semesters: {
-          first: "",
-          second: "",
-        },
-      },
-      {
-        yearNumber: 3,
-        semesters: {
-          first: "",
-          second: "",
-        },
-      },
-      {
-        yearNumber: 4,
-        semesters: {
-          first: "",
-          second: "",
-        },
-      },
+      { yearNumber: 1, semesters: { first: "", second: "" } },
+      { yearNumber: 2, semesters: { first: "", second: "" } },
+      { yearNumber: 3, semesters: { first: "", second: "" } },
+      { yearNumber: 4, semesters: { first: "", second: "" } },
+    ],
+  },
+
+  {
+    year: 2025,
+    label: "دفعة قوى 2025",
+    years: [
+      { yearNumber: 1, semesters: { first: "", second: "" } },
+      { yearNumber: 2, semesters: { first: "", second: "" } },
+      { yearNumber: 3, semesters: { first: "", second: "" } },
+      { yearNumber: 4, semesters: { first: "", second: "" } },
+    ],
+  },
+
+  {
+    year: 2024,
+    label: "دفعة قوى 2024",
+    years: [
+      { yearNumber: 1, semesters: { first: "", second: "" } },
+      { yearNumber: 2, semesters: { first: "", second: "" } },
+      { yearNumber: 3, semesters: { first: "", second: "" } },
+      { yearNumber: 4, semesters: { first: "", second: "" } },
+    ],
+  },
+
+  {
+    year: 2023,
+    label: "دفعة قوى 2023",
+    years: [
+      { yearNumber: 1, semesters: { first: "", second: "" } },
+      { yearNumber: 2, semesters: { first: "", second: "" } },
+      { yearNumber: 3, semesters: { first: "", second: "" } },
+      { yearNumber: 4, semesters: { first: "", second: "" } },
     ],
   },
 ];
