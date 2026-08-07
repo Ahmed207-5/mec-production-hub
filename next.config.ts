c3861c98ba72727a928134cf7e3745d6c90c3818
Ahmed El-Saeed <ahmdmydw981@gmail.com>
2026-08-07 22:35:12 +0300

    Remove PWA

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
-import withPWA from "@ducanh2912/next-pwa";
+import type { NextConfig } from "next";
 
-const nextConfig = withPWA({
-  dest: "public",
-  register: true,
-  disable: process.env.NODE_ENV === "development",
-});
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
