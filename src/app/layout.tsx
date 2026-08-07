5ec30af6754e56a4d4ad04bfcdf96fb63c9cca58
Ahmed El-Saeed <ahmdmydw981@gmail.com>
2026-08-07 22:26:29 +0300

    Add install prompt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,20 +21,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ar" dir="rtl" className="h-full antialiased">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Cairo:wght@600;700;800&family=IBM+Plex+Sans+Arabic:wght@400;500;600&display=swap"
-        />
-      </head>
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="ar" dir="rtl">
+      <body>
+        {children}
+        <InstallPrompt />
+      </body>
     </html>
   );
 }
