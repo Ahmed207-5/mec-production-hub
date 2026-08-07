f1fb2aa8ed35ddbcff1b829a546bac18c435e494
Ahmed El-Saeed <ahmdmydw981@gmail.com>
2026-08-07 22:18:49 +0300

    Add Power 2025 drive links

--- src/data/power.ts
+++ src/data/power.ts
@@ -24,16 +24,47 @@ export const batches: Batch[] = [
   },
 
   {
-    year: 2025,
-    label: "دفعة قوى 2025",
-    years: [
-      { yearNumber: 1, semesters: { first: "", second: "" } },
-      { yearNumber: 2, semesters: { first: "", second: "" } },
-      { yearNumber: 3, semesters: { first: "", second: "" } },
-      { yearNumber: 4, semesters: { first: "", second: "" } },
-    ],
-  },
-
+  year: 2025,
+  label: "دفعة قوى 2025",
+  years: [
+    {
+      yearNumber: 1,
+      semesters: {
+        first:
+          "https://drive.google.com/drive/folders/179EPTKGcrYeoyBnEXQq3vu8FYVBDbNij?usp=drive_link",
+        second:
+          "https://drive.google.com/drive/folders/179VWs5LwmyzYWc0xE6HRM4FC9Um9y_yH?usp=drive_link",
+      },
+    },
+    {
+      yearNumber: 2,
+      semesters: {
+        first:
+          "https://drive.google.com/drive/folders/19PuK2iX6W3_V-p3v87Mi0JTENygklZrZ?usp=drive_link",
+        second:
+          "https://drive.google.com/drive/folders/17CxDcvchB0B9Lvjo6ZraTcrRpgDpx-Pe?usp=drive_link",
+      },
+    },
+    {
+      yearNumber: 3,
+      semesters: {
+        first:
+          "https://drive.google.com/drive/folders/1yswnsNbSFBIbe89MKTnsSM1tJvwyIjIs?usp=drive_link",
+        second:
+          "https://drive.google.com/drive/folders/1TRQ9GtkO91KxUkdKUbACYBDdg6ItXpBH?usp=drive_link",
+      },
+    },
+    {
+      yearNumber: 4,
+      semesters: {
+        first:
+          "https://drive.google.com/drive/folders/12mgUNJPnyJZGtm73v1V2aQD2B182buHQ?usp=drive_link",
+        second:
+          "https://drive.google.com/drive/folders/1t-wOoKDSnHU3N__2Znuj1bvor66yqewA?usp=drive_link",
+      },
+    },
+  ],
+},
   {
     year: 2024,
     label: "دفعة قوى 2024",
